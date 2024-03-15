7272aba8781942646662ed817eede634ba7638c1
MahmoodOdeh <odeh.mahmood3@gmail.com>
2024-03-16 01:36:02 +0200

    fix yml

--- test/tankerkoenig_stats_api_test.py
+++ test/tankerkoenig_stats_api_test.py
@@ -1,9 +1,6 @@
 import unittest
 from concurrent.futures.thread import ThreadPoolExecutor
-import sys
 
-sys.path.append('AirbnbSeleniumGridProject\\\\infra')
-sys.path.append('AirbnbSeleniumGridProject\\\\logic')
 
 from infra.api_wrapper import APIWrapper
 from infra.browser_wrapper import BrowserWrapper
